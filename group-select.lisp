170f2f7ee1f78bb0056c7073a67c0c5bd9378cb6
root <root@joel-laptop.(none)>
2009-06-09 18:57:18 +0530

    Event handlers -------------- - exposure - click - toggle
    TODO: integrate with stumpwm

--- group-select.lisp
+++ group-select.lisp
@@ -4,11 +4,11 @@
 (defparameter *cubes* (make-array 1 :fill-pointer 0 :adjustable t))
 
 (defstruct cube
-   state
-   number
-   window
-   gcontext-normal
-   gcontext-toggled)
+  state
+  number
+  window
+  gcontext-normal
+  gcontext-toggled)
 
 (defun create-cubes ()
   (dotimes (n 10)
@@ -50,18 +50,29 @@
 	       :number num
 	       :window win
 	       :gcontext-normal gcontext-normal
-	       :gcontext-toggled gcontext-normal)))
+	       :gcontext-toggled gcontext-toggled)))
 
-;;; cube events ;;;
+(defun toggle-cube (cube)
+  (cond ((eq (cube-state cube) :normal)
+	 (setf (cube-state cube) :toggled))
+	((eq (cube-state cube) :toggled)
+	 (setf (cube-state cube) :normal))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; cube events 			      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; click
 (defun cube-clicked (cube)
-  (dformat 0 "cube ~a clicked~%" (cube-number cube)))
+  (dformat 0 "cube ~a clicked~%" (cube-number cube))
+  (toggle-cube cube)
+  (draw-cube cube))
 
 ;; exposure
 (defun draw-cube (cube)
   (let* ((win (cube-window cube))
-	 (gc  (cube-gcontext-normal cube))
+	 (gc  (or (and (eq (cube-state cube) :toggled) (cube-gcontext-toggled cube))
+		  (cube-gcontext-normal cube)))
 	 (font (xlib:gcontext-font gc))
 	 (string (write-to-string (cube-number cube))))
     ;; sync window background with gc background
@@ -73,116 +84,16 @@
 			     (+ (xlib:font-ascent font) 1)
 			     string
 			     :translate #'translate-id 
-			     :size 16))
-  (xlib:display-finish-output *display*)
-  t)
+			     :size 16)
+    (xlib:display-finish-output *display*)
+    gc))
 
 
-;; helper functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; helper functions 			      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun find-cube-window (win)
   (find-if (lambda (cube)
 	     (eq (cube-window cube) win))
 	   *cubes*))
-
-(defun cube-test ()
-  (let* (cube (create-cube 10))
-    (draw-cube cube)))
-
-;; (xlib:event-case (display :force-output-p t
-;; 			      :discard-p t)
-;;   (:exposure (count)
-;; 		 (xlib:draw-rectangle
-;; 		  window
-;; 		  (get (rect-c rect) 'grackon)
-;; 		  (rect-x rect)
-;; 		  (rect-y rect)
-;; 		  (rect-w rect)
-;; 		  (rect-h rect)
-;; 		  'fill)))
-;; nil))))
-	
-
-;; (defun show-rectangles (window width height)
-;;   (let* ((display stumpwm:*display*)
-;; 	 (screen (first (xlib:display-roots display)))
-;; 	 (black (xlib:screen-black-pixel screen))
-;; 	 (white (xlib:screen-white-pixel screen))
-;; 	 (my-window (xlib:create-window
-;; 		     :parent window
-;; 		     :x 0
-;; 		     :y 0
-;; 		     :width width
-;; 		     :height height
-;; 		     :background black
-;; 		     :event-mask (xlib:make-event-mask :exposure))))
-;;     (dolist (colour-symbol *colour-list*)
-;;       (setf (get colour-symbol 'grackon)
-;; 	    (xlib:create-gcontext
-;; 	     :drawable root-window
-;; 	     :foreground (xlib:alloc-color
-;; 			  (xlib:window-colormap root-window)
-;; 			  (symbol-name colour-symbol))
-;; 	     :background black)))
-;;     (xlib:map-window my-window)
-;;     (xlib:event-case (display :force-output-p t
-;; 			      :discard-p t)
-;;       (:exposure (count)
-;; 		 (when (zerop count)
-;; 		   (dolist (rect *rect-list*)
-;; 		     (xlib:draw-rectangle
-;; 		      window
-;; 		      (get (rect-c rect) 'grackon)
-;; 		      (rect-x rect)
-;; 		      (rect-y rect)
-;; 		      (rect-w rect)
-;; 		      (rect-h rect)
-;; 		      'fill)))
-;; 		 nil)
-;;       (:button-press () t))
-;;     my-window))
-
-
-;; (defstruct rect x y w h c)
-
-;; (defun random-choice (item-list)
-;;   (let ((options (length item-list)))
-;;     (elt item-list (random options))))
-
-;; (defun cons-up-rect-list (n)
-;;   (cons-up n #'(lambda()
-;; 		 (make-rect :x (random 300)
-;; 			    :y (random 300)
-;; 			    :w (+ 20 (random 30))
-;; 			    :h (+ 20 (random 30))
-;; 			    :c (random-choice *colour-list*)))))
-
-;; (defun cons-up (count constructor)
-;;   (let (accumulator)
-;;     (dotimes (index count accumulator)
-;;       (push (funcall constructor) accumulator))))
-
-
-;; (defparameter *default-rect-list*
-;;   (cons-up-rect-list 10))
-
-;; (defvar *rect-list* *default-rect-list*)
-;; ; The program can be loaded and run, and will display
-;; ; some rectangles. If you have already created some 
-;; ; rectangles, it will not clobber them.
-;; ; In particular you can edit the source, reload the 
-;; ; the file, and you still have your own rectangle list.
-;; ; At any time, you can get back to the default with
-;; ; (setf *rect-list* *default-rect-list*)
-    ;; (xlib:create-gcontext :drawable win
-    ;; 					 :font font
-    ;; 					 :foreground fg
-    ;; 					 :background bg)))
-
-;; (xlib:draw-glyphs *rect* grackon 5 5 "hello"
-;; 		      :translate #'translate-id
-;; 		      :size 16)
-
-;; (grackon (xlib:create-gcontext
-;; 	   :drawable *rect*
-;; 	   :foreground black
-;; 	   :background white)))
